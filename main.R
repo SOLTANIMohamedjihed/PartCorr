5a769b7ad316d03e7b76fc8046fc45f5f6291574
SOLTANI Mohamed Jihed <87778186+SOLTANIMohamedjihed@users.noreply.github.com>
2023-04-10 11:52:51 +0100

    Update main.R

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library(BrainCon)
 # data ? popsimA
 data(popsimA)
 data(popsimB)
-# estimating partial correlation coefficients by lasso (scaled lasso does the same)
+# estimating partial correlation coefficients by lasso 
 E1 <- population.est(popsimA, type = 'l')
 E2 <- population.est(popsimB, type = 'l')
 # Identify differences of partial correlations between two populations
